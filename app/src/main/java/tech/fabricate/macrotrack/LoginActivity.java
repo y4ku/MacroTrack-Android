81a87617414b62b304d1b124dae2e84b9eda963e
Jakub Misterka <san.y4ku@gmail.com>
2015-11-22 17:37:19 -0600

    SignUp :: can toggle back and forth

--- app/src/main/java/tech/fabricate/macrotrack/LoginActivity.java
+++ app/src/main/java/tech/fabricate/macrotrack/LoginActivity.java
@@ -1,12 +1,14 @@
 package tech.fabricate.macrotrack;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -21,9 +23,12 @@ import tech.fabricate.macrotrack.rest.service.LoginService;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int REQUEST_SIGNUP = 0;
+
     private EditText emailInput;
     private EditText passwordInput;
     private Button loginButton;
+    private TextView signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         emailInput = (EditText) findViewById(R.id.loginEmail);
         passwordInput = (EditText) findViewById(R.id.loginPassword);
         loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (TextView) findViewById(R.id.signUpButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +45,15 @@ public class LoginActivity extends AppCompatActivity {
                 login(emailInput.getText().toString(), passwordInput.getText().toString());
             }
         });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+
     }
 
     public void login(String username, String password) {
--- app/src/main/java/tech/fabricate/macrotrack/SignupActivity.java
+++ app/src/main/java/tech/fabricate/macrotrack/SignupActivity.java
@@ -1,9 +1,30 @@
 package tech.fabricate.macrotrack;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by y4ku on 11/22/15.
  */
 public class SignupActivity extends AppCompatActivity {
+
+    private TextView loginLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        loginLink = (TextView) findViewById(R.id.loginLink);
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    };
+
 }
